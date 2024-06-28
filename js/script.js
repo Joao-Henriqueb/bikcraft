3a15435daa74b699f040e3826d9c37a76160599f
João Henrique <joaohenriquerbueno@gmail.com>
2024-06-28 12:14:04 -0300

    passado informação dos links para formulario caso for seguro ou bicicleta

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
+// ativar links do Menu
 const links = document.querySelectorAll('.header-menu a');
-
 function ativarLink(link) {
   const url = location.href;
   const href = link.href;
@@ -8,5 +8,15 @@ function ativarLink(link) {
     link.classList.add('ativo');
   }
 }
-
 links.forEach(ativarLink);
+
+//ativar itens do orçamento
+
+const paramentros = new URLSearchParams(location.search);
+function ativarProduto(parametro) {
+  if (paramentros) {
+    const elemento = document.getElementById(parametro);
+    elemento.checked = true;
+  }
+}
+paramentros.forEach(ativarProduto);
