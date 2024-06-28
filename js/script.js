0a3dca8a0b9ab5bd64211586be2acd7689b4b84e
João Henrique <joaohenriquerbueno@gmail.com>
2024-06-28 13:56:35 -0300

    adicionado interatividade na galeria de fotos

--- js/script.js
+++ js/script.js
@@ -31,11 +31,27 @@ function ativaerPergunta(event) {
   resposta.classList.toggle('ativa');
   const ativa = resposta.classList.contains('ativa');
   pergunta.setAttribute('aria-expanded', ativa);
-
-  console.log(pergunta);
 }
 
 function eventoPerguntas(pergunta) {
   pergunta.addEventListener('click', ativaerPergunta);
 }
 perguntas.forEach(eventoPerguntas);
+
+// Galeria de Bicicleta
+const galeria = document.querySelectorAll('.bicicleta-imagens img');
+const galeriaContainer = document.querySelector('.bicicleta-imagens');
+
+function trocarImagen(event) {
+  const img = event.currentTarget;
+  const media = matchMedia('(min-width:1000px)').matches;
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
+}
+
+function eventosGaleria(img) {
+  img.addEventListener('click', trocarImagen);
+}
+
+galeria.forEach(eventosGaleria);
